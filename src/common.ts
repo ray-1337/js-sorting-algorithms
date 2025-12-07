2a358ea273b23977767a14475cee27f5ef646e8e
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-12-07 09:14:45 +0800

    Add a function to check if items in the list is sorted

--- src/common.ts
+++ src/common.ts
@@ -4,6 +4,10 @@ export function cloneArray<T>(array: T[]) {
   return [...array];
 };
 
+export function isSorted<T>(array: T[], compareFn: CompareFunction<T> = createDefaultCompare()) {
+  return array.every((item, index, arr) => index > 0 ? (compareFn(arr[index - 1], item) <= 0) : true);
+};
+
 export function createDefaultCompare<T>(): CompareFunction<T> {
   return (a, b) => {
     return (a > b) ? 1 : (a < b ? - 1 : 0);
