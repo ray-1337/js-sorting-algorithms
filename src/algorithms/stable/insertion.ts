ce103d033d649ad0adf9ee804a3acbc3f8109ed2
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-12-07 19:20:15 +0800

    Fix using wrong array during comparison in Insertion Sort

--- src/algorithms/stable/insertion.ts
+++ src/algorithms/stable/insertion.ts
@@ -7,7 +7,7 @@ export default function insertionSort<T>(arr: T[], compareFn: CompareFunction<T>
   for (let i = 1; i < total; i++) {
     let [key, j]: [T, number] = [array[i], i - 1]
 
-    while (j >= 0 && compareFn(arr[j], key) > 0) {
+    while (j >= 0 && compareFn(array[j], key) > 0) {
       array[j + 1] = array[j];
       j = j - 1;
     };
