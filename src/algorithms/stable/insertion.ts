d9faa6172bc1243ddd679b3f9600c1faf333b613
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2025-12-07 15:30:29 +0800

    Add a final version of stable sort: insertion sort

---
+++ src/algorithms/stable/insertion.ts
@@ -0,0 +1,20 @@
+import type { CompareFunction } from "../../typings/index";
+import { cloneArray, createDefaultCompare } from "../../common";
+
+// Lines of code were copied from https://www.geeksforgeeks.org/dsa/insertion-sort-algorithm and optimized
+export default function insertionSort<T>(arr: T[], compareFn: CompareFunction<T> = createDefaultCompare()) {
+  const [array, total]: [T[], number] = [cloneArray(arr), arr.length];
+
+  for (let i = 1; i < total; i++) {
+    let [key, j]: [T, number] = [array[i], i - 1]
+
+    while (j >= 0 && compareFn(arr[j], key) > 0) {
+      array[j + 1] = array[j];
+      j = j - 1;
+    };
+
+    array[j + 1] = key;
+  };
+
+  return array;
+};
